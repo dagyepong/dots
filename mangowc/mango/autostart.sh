933ca4422a39fcb03c4c84e0b0036b0078aa45ff
dagyepong <dennisagyepong2@gmail.com>
2025-11-13 15:17:41 -0600

    new

---
+++ mangowc/mango/autostart.sh
@@ -0,0 +1,17 @@
+waybar &
+swaybg -i $HOME/Pictures/wallpapers/Anime-Girl1.png &
+mako &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+# The next line of command is not necessary. It is only to avoid some situations where it cannot start automatically
+# /usr/lib/xdg-desktop-portal-wlr &
+nm-applet >/dev/null &
+kdeconnect-indicator &
+easyeffects --gapplication-service &
+# keep clipboard content
+wl-clip-persist --clipboard regular --reconnect-tries 0 &
+# clipboard content manager
+clipse -listen &
+# wl-paste --type text --watch cliphist store & 
+# wl-paste --type image --watch cliphist store & 
+# Permission authentication
+/usr/lib/xfce-polkit/xfce-polkit &
