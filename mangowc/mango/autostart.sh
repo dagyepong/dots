5d95139750e29d63b54bb9d96b8bcd925eba3f29
dagyepong <dennisagyepong2@gmail.com>
2025-11-13 15:57:17 -0600

    new

--- mangowc/mango/autostart.sh
+++ mangowc/mango/autostart.sh
@@ -9,11 +9,31 @@ dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 nm-applet >/dev/null &
 kdeconnect-indicator &
 easyeffects --gapplication-service &
+
+
+
 # keep clipboard content
-wl-clip-persist --clipboard regular --reconnect-tries 0 &
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
+
+# clipboard content manager
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
+
 # clipboard content manager
 clipse -listen &
 # wl-paste --type text --watch cliphist store & 
 # wl-paste --type image --watch cliphist store & 
 # Permission authentication
 /usr/lib/xfce-polkit/xfce-polkit &
+
+
+# inhibit by audio
+sway-audio-idle-inhibit >/dev/null 2>&1 &
+
+# change light value and volume value by swayosd-client in keybind
+swayosd-server >/dev/null 2>&1 &
+
+# lock
+
+swayidle -w \
+    timeout 300 'swaylock -f --effect-blur 5x5 --fade-in 1' \
+    before-sleep 'swaylock -f --effect-blur 5x5 --fade-in 1'  >/dev/null 2>&1 & 
