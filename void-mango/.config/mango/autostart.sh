26ba779b7be1db2f127c8304eaf0233c6e3c99a4
dagyepong <dennisagyepong2@gmail.com>
2025-11-14 19:54:23 -0600

    new

--- void-mango/.config/mango/autostart.sh
+++ void-mango/.config/mango/autostart.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
 
-dbus-update-activation-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
-kanshi &
+waybar &
+swaybg -i $HOME/Pictures/wallpapers/Anime-Girl1.png &
+mako &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+# The next line of command is not necessary. It is only to avoid some situations where it cannot start automatically
+# /usr/lib/xdg-desktop-portal-wlr &
+nm-applet >/dev/null &
+kdeconnect-indicator &
+easyeffects --gapplication-service &
+# clipboard content manager
+clipse -listen &
+# wl-paste --type text --watch cliphist store & 
+# wl-paste --type image --watch cliphist store & 
+# Permission authentication
+/usr/lib/xfce-polkit/xfce-polkit &
+
+
 swayidle_start.sh &
 sway-audio-idle-inhibit &
 # xwayland dpi scale
@@ -12,9 +27,6 @@ pipewire &
 #/usr/libexec/polkit-gnome-authentication-agent-1 &
 soteria &
 
-dunst -conf ~/.config/dunst/dunstrc_mango &
-waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css &
-/home/ron/.config/mango/waybar/shadow/panel-shadow.py &
 wl-clip-persist --clipboard regular --reconnect-tries 0 &
 wl-paste --type text --watch cliphist store & 
 
@@ -32,6 +44,4 @@ udiskie --smart-tray &
 steam %U &
 heroic &
 
-thunderbird &
-/home/ron/.local/share/flatpak/exports/bin/ca.andyholmes.Valent &
 foot -T logs --app-id logs -e tmuxinator start logs_mango -n logs &
